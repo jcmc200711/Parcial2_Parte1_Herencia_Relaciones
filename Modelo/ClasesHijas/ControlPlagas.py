e5b069ab58ba81a3c39f273d221e8e592bace1c2
Natalia Gallego <natalia.gallego@utp.edu.co>
2026-05-05 01:21:33 -0500

    Refactor __init__ method parameter naming

--- Modelo/ClasesHijas/ControlPlagas.py
+++ Modelo/ClasesHijas/ControlPlagas.py
@@ -7,6 +7,6 @@ sys.path.append(ruta_padre)
 from ClasesPadre.ProductoDeControl import ProductoControl
 
 class ControlPlagas(ProductoControl):
-    def __init__(self, _registroICA, nombre, frecuencia, valor, periodo_carencia = 1):
-        super().__init__(_registroICA, nombre, frecuencia, valor)
+    def __init__(self, registroICA, nombre, frecuencia, valor, periodo_carencia):
+        super().__init__(registroICA, nombre, frecuencia, valor)
         self.periodo_carencia = periodo_carencia
