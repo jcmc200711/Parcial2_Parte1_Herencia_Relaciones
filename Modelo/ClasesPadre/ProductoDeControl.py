5780095d4b82bd83dc14726e4daa3d56346c9be5
Natalia Gallego <natalia.gallego@utp.edu.co>
2026-05-05 01:23:35 -0500

    Update ProductoDeControl.py

--- Modelo/ClasesPadre/ProductoDeControl.py
+++ Modelo/ClasesPadre/ProductoDeControl.py
@@ -1,8 +1,6 @@
 class ProductoControl:
-    __registros_ICA = 0
-    def __init__(self, nombre = "Producto default", frecuencia = 1, valor = 1000):
-        ProductoControl.__registros_ICA+=1
-        self._registroICA = ProductoControl.__registros_ICA
+    def __init__(self, registroICA, nombre, frecuencia, valor):
+        self._registroICA = registroICA
         self.nombre = nombre
         self.frecuencia = frecuencia
         self.valor = valor
