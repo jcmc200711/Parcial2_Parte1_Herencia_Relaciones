72edf8f16f3bb3f164c54fd9a3970bab8d5a7e3c
Natalia Gallego <natalia.gallego@utp.edu.co>
2026-05-05 01:24:48 -0500

    Refactor menu_opciones for better input handling
    Refactor menu_opciones function to improve user prompts and input validation.

--- UI/menu_opciones.py
+++ UI/menu_opciones.py
@@ -1,12 +1,18 @@
-from limpiarPantalla import limpiar_pantalla
-def menu_opciones(mensaje,alternativas):
-    for alternativa in alternativas : print(alternativa)
-    while(True):
-        opcion = input(mensaje)
-        if(opcion.isdigit() and (opcion >= 1 and opcion <= len(alternativas))):
+from UI.limpiarPantalla import limpiar_pantalla
+
+def menu_opciones(mensaje, alternativas):
+     print(mensaje)   
+
+     for alternativa in alternativas:
+        print(alternativa)
+
+     while True:
+        opcion = input("Seleccione una opción: ")
+
+        if opcion.isdigit():
             opcion = int(opcion)
-            break
-        else:
-            print("Ingrese una opción válida...\n")
-            limpiar_pantalla()
-    return opcion
+            if 1 <= opcion <= len(alternativas):
+                return opcion
+
+        print("Ingrese una opción válida...\n")
+        limpiar_pantalla()
