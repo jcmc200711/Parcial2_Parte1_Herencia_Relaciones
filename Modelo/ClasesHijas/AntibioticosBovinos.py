5c3748229ad9af41f318d4f549f15aa546b4a01b
Natalia Gallego <natalia.gallego@utp.edu.co>
2026-05-05 01:51:13 -0500

    Update AntibioticosBovinos.py

--- Modelo/ClasesHijas/AntibioticosBovinos.py
+++ Modelo/ClasesHijas/AntibioticosBovinos.py
@@ -3,12 +3,8 @@ import os
 ruta_padre = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(ruta_padre)
 
-
 from ClasesPadre.Antibiotico import Antibiotico
 
 class AntibioticoBovino(Antibiotico):
     def __init__(self, nombre, dosis, precio):
         super().__init__(nombre, dosis, "bovino", precio)
-        
-            
-        
