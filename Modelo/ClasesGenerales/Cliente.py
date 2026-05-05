cb2a90bd6247f7950dde59fbc3a5e441984382b4
Natalia Gallego <natalia.gallego@utp.edu.co>
2026-05-05 01:16:48 -0500

    Correct client count increment in Cliente class

--- Modelo/ClasesGenerales/Cliente.py
+++ Modelo/ClasesGenerales/Cliente.py
@@ -1,7 +1,7 @@
 class Cliente:
     _cantidad_clientes = 0
     def __init__(self, nombre = "Cliente default", cedula = "00000"):
-        _cantidad_clientes += 1
+        Cliente._cantidad_clientes += 1
         self.nombre = nombre
         self.cedula = cedula
         self._cant_comprar = 0
